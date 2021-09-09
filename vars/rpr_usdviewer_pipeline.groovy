9e8afce44b5cdb04fe23d860b67800b520cd619f
Ilia Nemankov <INemankov@luxoft.com>
2021-09-09 20:09:15 +0300

    Do not clone submodules on PreBuilt stage

--- vars/rpr_usdviewer_pipeline.groovy
+++ vars/rpr_usdviewer_pipeline.groovy
@@ -700,7 +700,7 @@ def executePreBuild(Map options) {
     if (!options['isPreBuilt']) {
 
         withNotifications(title: "Jenkins build configuration", options: options, configuration: NotificationConfiguration.DOWNLOAD_SOURCE_CODE_REPO) {
-            checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, submoduleDepth: 1)
+            checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, disableSubmodules: false)
         }
 
         options.commitAuthor = utils.getBatOutput(this, "git show -s --format=%%an HEAD ")
