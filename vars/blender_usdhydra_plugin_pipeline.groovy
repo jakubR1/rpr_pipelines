333b7cbf9797e8f5d90566abbeb0804ea54dd7d3
Alexey Kozlov <lexainb3@gmail.com>
2021-10-08 10:00:50 +0300

    Check test block

--- vars/blender_usdhydra_plugin_pipeline.groovy
+++ vars/blender_usdhydra_plugin_pipeline.groovy
@@ -539,11 +539,11 @@ def getReportBuildArgs(String engineName, Map options) {
 def executePreBuild(Map options)
 {
     // manual job with prebuilt plugin
-    if (options.isPreBuilt) {
+    if (options['isPreBuilt']) {
         println "[INFO] Build was detected as prebuilt. Build stage will be skipped"
         currentBuild.description = "<b>Project branch:</b> Prebuilt plugin<br/>"
-        options.executeBuild = false
-        options.executeTests = true
+        options['executeBuild'] = false
+        options['executeTests'] = true
     // manual job
     } else if (options.forceBuild) {
         println "[INFO] Manual job launch detected"
@@ -553,9 +553,9 @@ def executePreBuild(Map options)
     } else {
         if (env.CHANGE_URL) {
             println "[INFO] Branch was detected as Pull Request"
-            options.executeBuild = true
-            options.executeTests = true
-            options.testsPackage = "regression.json"
+            options['executeBuild'] = true
+            options['executeTests'] = true
+            options['testsPackage'] = "regression.json"
         } else if (env.BRANCH_NAME == "master" || env.BRANCH_NAME == "develop") {
            println "[INFO] ${env.BRANCH_NAME} branch was detected"
            options['executeBuild'] = true
@@ -663,102 +663,8 @@ def executePreBuild(Map options)
 
     withNotifications(title: "Jenkins build configuration", options: options, configuration: NotificationConfiguration.CONFIGURE_TESTS) {
         dir('jobs_test_usdblender') {
-            checkoutScm(branchName: options.testsBranch, repositoryUrl: options.testRepo)
-
-            options['testsBranch'] = bat (script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
-            dir('jobs_launcher') {
-                options['jobsLauncherBranch'] = bat (script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
-            }
-            println "[INFO] Test branch hash: ${options['testsBranch']}"
-
-            def packageInfo
-
-            if (options.testsPackage != "none") {
-                packageInfo = readJSON file: "jobs/${options.testsPackage}"
-                options.isPackageSplitted = packageInfo["split"]
-                // if it's build of manual job and package can be splitted - use list of tests which was specified in params (user can change list of tests before run build)
-                if (options.forceBuild && options.isPackageSplitted && options.tests) {
-                    options.testsPackage = "none"
-                }
-            }
-
-            if (options.testsPackage != "none") {
-                def tempTests = []
-
-                if (options.isPackageSplitted) {
-                    println("[INFO] Tests package '${options.testsPackage}' can be splitted")
-                } else {
-                    // save tests which user wants to run with non-splitted tests package
-                    if (options.tests) {
-                        tempTests = options.tests.split(" ") as List
-                    }
-                    println("[INFO] Tests package '${options.testsPackage}' can't be splitted")
-                }
-
-                // modify name of tests package if tests package is non-splitted (it will be use for run package few time with different engines)
-                String modifiedPackageName = "${options.testsPackage}~"
-                options.groupsUMS = tempTests.clone()
-                packageInfo["groups"].each() {
-                    if (options.isPackageSplitted) {
-                        tempTests << it.key
-                        options.groupsUMS << it.key
-                    } else {
-                        if (tempTests.contains(it.key)) {
-                            // add duplicated group name in name of package group name for exclude it
-                            modifiedPackageName = "${modifiedPackageName},${it.key}"
-                        } else {
-                            options.groupsUMS << it.key
-                        }
-                    }
-                }
-
-                options.tests = utils.uniteSuites(this, "jobs/weights.json", tempTests)
-                options.tests.each() {
-                    def xml_timeout = utils.getTimeoutFromXML(this, "${it}", "simpleRender.py", options.ADDITIONAL_XML_TIMEOUT)
-                    options.timeouts["${it}"] = (xml_timeout > 0) ? xml_timeout : options.TEST_TIMEOUT
-                }
-                options.engines.each { engine ->
-                    options.tests.each() {
-                        tests << "${it}-${engine}"
-                    }
-                }
-
-                modifiedPackageName = modifiedPackageName.replace('~,', '~')
-
-                if (options.isPackageSplitted) {
-                    options.testsPackage = "none"
-                } else {
-                    options.testsPackage = modifiedPackageName
-                    options.engines.each { engine ->
-                        tests << "${modifiedPackageName}-${engine}"
-                    }
-                    options.timeouts[options.testsPackage] = options.NON_SPLITTED_PACKAGE_TIMEOUT + options.ADDITIONAL_XML_TIMEOUT
-                }
-            } else if (options.tests) {
-                options.groupsUMS = options.tests.split(" ") as List
-                options.tests = utils.uniteSuites(this, "jobs/weights.json", options.tests.split(" ") as List)
-                options.tests.each() {
-                    def xml_timeout = utils.getTimeoutFromXML(this, it, "simpleRender.py", options.ADDITIONAL_XML_TIMEOUT)
-                    options.timeouts["${it}"] = (xml_timeout > 0) ? xml_timeout : options.TEST_TIMEOUT
-                }
-                options.engines.each { engine ->
-                    options.tests.each() {
-                        tests << "${it}-${engine}"
-                    }
-                }
-            } else {
-                options.executeTests = false
-            }
-            options.tests = tests
+            configureTestBlock(options)
         }
-
-        if (env.CHANGE_URL) {
-            options.githubNotificator.initChecks(options, "${BUILD_URL}")
-        }
-
-        options.testsList = options.tests
-
-        println "timeouts: ${options.timeouts}"
     }
 
     if (options.flexibleUpdates && multiplatform_pipeline.shouldExecuteDelpoyStage(options)) {
