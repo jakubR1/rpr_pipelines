a9bdc36f871cea44ca7e8a0d0fd0e508681da0eb
Ilia Nemankov <INemankov@luxoft.com>
2021-10-12 12:58:32 +0300

    Fix package scripts name

--- vars/rpr_hybrid_ue_pipeline.groovy
+++ vars/rpr_hybrid_ue_pipeline.groovy
@@ -91,7 +91,7 @@ def executeBuildWindows(String projectName, Map options) {
 
     // the last script can return non-zero exit code, but build can be ok
     try {
-        bat("4_${projectName}.bat > \"4_${projectName}.log\" 2>&1")
+        bat("4_Package_${projectName}.bat > \"4_Package_${projectName}.log\" 2>&1")
     } catch (e) {
         println(e.getMessage())
     }
