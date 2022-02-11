190b7ef69ced801bb33a95adf8621827f5a86783
Wieceslaw <slava.astra@yahoo.com>
2022-02-11 03:13:19 +0300

    Failed builds bug fix

--- vars/amdDriverUpdate.groovy
+++ vars/amdDriverUpdate.groovy
@@ -1,5 +1,3 @@
-DRIVER_PAGE_URL = "https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6800-series/amd-radeon-rx-6800-xt"
-
 def main(Map options) {
     timestamps {
         def updateTasks = [:]
@@ -13,11 +11,21 @@ def main(Map options) {
                 stage("Driver update ${it}") {
                     node("${it}") {
                         timeout(time: "30", unit: "MINUTES") {
-                            bat "${CIS_TOOLS}\\amd_request.bat \"${DRIVER_PAGE_URL}\" ${env.WORKSPACE}\\page.html >> page_download_${it}.log 2>&1 "
+                            try {
+                                DRIVER_PAGE_URL = "https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6800-series/amd-radeon-rx-6800-xt"
+                                
+                                bat "${CIS_TOOLS}\\driver_detection\\amd_request.bat \"${DRIVER_PAGE_URL}\" ${env.WORKSPACE}\\page.html >> page_download_${it}.log 2>&1 "
 
-                            withEnv(["PATH=c:\\python39\\;c:\\python39\\scripts\\;${PATH}"]) {
-                                bat "python -m pip install -r ${CIS_TOOLS}\\driver_detection\\requirements.txt >> parse_stage_${it}.log 2>&1"
-                                bat "python ${CIS_TOOLS}\\parse_driver.py --file ${env.WORKSPACE}\\page.html --output ${env.WORKSPACE}\\driver.exe >> parse_stage_${it}.log 2>&1"
+                                withEnv(["PATH=c:\\python39\\;c:\\python39\\scripts\\;${PATH}"]) {
+                                    bat "python -m pip install -r ${CIS_TOOLS}\\driver_detection\\requirements.txt >> parse_stage_${it}.log 2>&1"
+                                    bat "python ${CIS_TOOLS}\\driver_detection\\parse_driver.py --file ${env.WORKSPACE}\\page.html --output ${env.WORKSPACE}\\driver.exe >> parse_stage_${it}.log 2>&1"
+                                }
+                            } catch(e) {
+                                println(e.toString());
+                                println(e.getMessage());
+                                currentBuild.result = "FAILURE";
+                            } finally {
+                                archiveArtifacts "*.log"
                             }
                         }
                     }
