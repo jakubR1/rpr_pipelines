8c6873bf54eb7aca474f25f0c7e0e1630334e1bb
Wieceslaw <slava.astra@yahoo.com>
2022-03-02 11:45:04 +0300

    Rename assets dir on nodes

--- vars/rpr_mayausd_pipeline.groovy
+++ vars/rpr_mayausd_pipeline.groovy
@@ -143,7 +143,7 @@ def executeTests(String osName, String asicName, Map options) {
         }
 
         withNotifications(title: options["stageName"], options: options, configuration: NotificationConfiguration.DOWNLOAD_SCENES) {
-            String assets_dir = isUnix() ? "${CIS_TOOLS}/../TestResources/usd_maya_autotests_assets" : "/mnt/c/TestResources/usd_maya_autotests_assets"
+            String assets_dir = isUnix() ? "${CIS_TOOLS}/../TestResources/usd_maya_autotests" : "/mnt/c/TestResources/usd_maya_autotests"
             downloadFiles("/volume1/Assets/usd_maya_autotests/", assets_dir)
         }
         try {
