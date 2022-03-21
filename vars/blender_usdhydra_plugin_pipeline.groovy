6f7c208c8b8ba2fd6bb0ef281faee450c1fbf238
Kozlov Alexey <36414392+Flame0n@users.noreply.github.com>
2022-03-21 21:31:37 +0100

    Added path to needed cmave version

--- vars/blender_usdhydra_plugin_pipeline.groovy
+++ vars/blender_usdhydra_plugin_pipeline.groovy
@@ -390,7 +390,7 @@ def executeBuildLinux(String osName, Map options) {
                     sh """
                         rm -rf ../bin
                         rm -rf ../libs
-                        export PATH=~/usr/cmake-path/bin:$PATH
+                        export PATH=~/usr/Cmake323/bin:$PATH
                         export OS=
                         python${it} --version >> ../${STAGE_NAME}_${it}.log  2>&1
                         python${it} -m pip install PySide2 >> ..\\${STAGE_NAME}_${it}.log  2>&1
@@ -403,7 +403,7 @@ def executeBuildLinux(String osName, Map options) {
                     }
                 } else {
                 sh """
-                        export PATH=~/usr/cmake-path/bin:$PATH
+                        export PATH=~/usr/Cmake323/bin:$PATH
                         export OS=
                         python${it} --version >> ../${STAGE_NAME}_${it}.log  2>&1
                         python${it} -m pip install PySide2 >> ..\\${STAGE_NAME}_${it}.log  2>&1
