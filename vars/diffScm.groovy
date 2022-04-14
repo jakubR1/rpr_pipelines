9faf50ff6bd27b79f53e0ef66cdf97eb9f7b8ae6
whatislove118 <oleg.pochikalin@mail.ru>
2022-04-14 11:11:46 +0300

    Update diffScm.groovy

--- vars/diffScm.groovy
+++ vars/diffScm.groovy
@@ -1,12 +1,12 @@
 import hudson.FilePath
 
 def call(){
-    def tree = [:].withDefault{ owner.call() }
-    TreeMap dependencies = tree
-    dependencies.WebUsdLiveServer.WebUsdAssetResolver = 'WebUsdAssetResolver/'
-    dependencies.WebUsdLiveServer.bbc = 'abc'
-    dependencies.WebUsdStreamServer.WebUsdAssetResolver = 'abc'
-    dependencies.WebUsdStreamServer.abc = 'abc'
+    // def tree = [:].withDefault{ owner.call() }
+    // TreeMap dependencies = tree
+    // dependencies.WebUsdLiveServer.WebUsdAssetResolver = 'WebUsdAssetResolver/'
+    // dependencies.WebUsdLiveServer.bbc = 'abc'
+    // dependencies.WebUsdStreamServer.WebUsdAssetResolver = 'abc'
+    // dependencies.WebUsdStreamServer.abc = 'abc'
 
     String changedFiles = sh (
         script: "git diff --dirstat=files,0 HEAD | sed 's/^[ 0-9.]+% //g'",
