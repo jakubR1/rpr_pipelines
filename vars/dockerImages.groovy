30193509aa0ba9189eb5ea6eaab22bacc033e72b
whatislove118 <oleg.pochikalin@mail.ru>
2022-04-15 18:14:39 +0300

    Update dockerImages.groovy

--- vars/dockerImages.groovy
+++ vars/dockerImages.groovy
@@ -3,7 +3,7 @@ def call(Map options){
     containersName = ['live', 'route', 'storage', 'stream', 'web']
     containersToBuild = []
     for (name in containersName){
-        cName = "$options.remoteHost/{$name}.$options.deployEnvironment"
+        cName = "$options.remoteHost/${name}.$options.deployEnvironment"
         res = sh(
             script: "docker images | grep $cName",
             returnStdout: true
