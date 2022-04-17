5c12810da465af5f9680b02e063d2d5ebca10bee
whatislove118 <oleg.pochikalin@mail.ru>
2022-04-17 14:34:01 +0300

    Update diffScm.groovy

--- vars/diffScm.groovy
+++ vars/diffScm.groovy
@@ -3,8 +3,7 @@ import hudson.FilePath
 def call(Map options){
     String changedProjects = sh (
         script: "git diff --name-only --right-only HEAD...$options.projectBranch | cut -d/ -f 1-1 | sort | uniq",
-        returnStdout: true,
-        returnStatus: true
+        returnStdout: true
     ).trim()
     println "[INFO] Changed projects:"
     println changedProjects
