81ea04c181ad270dd094d34bdbf61fb29da2c81a
Speedwag00n <INemankov@luxoft.com>
2022-04-19 14:51:23 +0300

    Fix condition

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -376,7 +376,7 @@ def makeDeploy(Map options, String engine = "") {
                     executeDeploy(options, platformList, testResultList)
                 }
 
-                if (engine && options.engines.size() > 0 && options.reportUpdater) {
+                if (engine && options.engines.size() > 1 && options.reportUpdater) {
                     options.reportUpdater.updateReport()
                 }
 
