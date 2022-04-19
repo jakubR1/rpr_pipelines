7754ae2ce7afec48c666e901b5569364c11a8568
whatislove118 <oleg.pochikalin@mail.ru>
2022-04-19 22:18:59 +0300

    Update diffScm.groovy

--- vars/diffScm.groovy
+++ vars/diffScm.groovy
@@ -8,7 +8,7 @@ def call(Map options){
     changedProjects = sh (
         script: "git diff --name-only --right-only HEAD...$options.projectBranch | cut -d/ -f 1-1 | sort | uniq",
         returnStdout: true
-    ).trim()
+    ).split("\n")
     println "[INFO] Changed projects:"
     println changedProjects
     return changedProjects
