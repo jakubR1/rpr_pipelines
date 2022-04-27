73eca19961ab36a92dd9b54d7ab0fd5f4dd273df
jakubR1 <jakub.rybski@dxc.com>
2022-04-27 18:01:13 +0200

    fix

--- src/GithubApiProvider.groovy
+++ src/GithubApiProvider.groovy
@@ -38,14 +38,11 @@ class GithubApiProvider {
 
     private String receiveInstallationToken(String organization_name) {
         try {
-            context.println("In recviceTK")
             context.withCredentials([context.string(credentialsId: "githubNotificationAppKey", variable: "GITHUB_APP_KEY"),
                 context.string(credentialsId: "githubNotificationAppId", variable: "GITHUB_APP_ID")]) {
                 context.withEnv(["GITHUB_APP_KEY=${context.GITHUB_APP_KEY}"]) {
                     if (context.isUnix()) {
-                        
                         installation_token = context.python3("${context.CIS_TOOLS}/auth_github.py --github_app_id ${context.GITHUB_APP_ID} --organization_name ${organization_name} --duration 540").split("\n")[-1]
-                        context.println("after token")
                     } else {
                         installation_token = context.python3("${context.CIS_TOOLS}\\auth_github.py --github_app_id ${context.GITHUB_APP_ID} --organization_name ${organization_name} --duration 540").split("\n")[-1]
                     }
