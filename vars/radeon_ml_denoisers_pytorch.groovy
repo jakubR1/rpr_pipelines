68b754e0b7a777ca7081351ecb78881159f806b6
jakubR1 <jakub.rybski@dxc.com>
2022-08-08 16:42:18 +0200

    try

--- vars/radeon_ml_denoisers_pytorch.groovy
+++ vars/radeon_ml_denoisers_pytorch.groovy
@@ -42,6 +42,8 @@ def executeTestCommand(String osName, String asicName, Map options)
 
                         sh  """
                                 expect sh/start_test.exp ${test} >> ../${STAGE_NAME}_${test}.log 2>&1
+                                pid=$!
+                                wait $pid
                             """
                             //utils.publishReport(this, BUILD_URL, "tested", "tested_${test}.html", "${test} report ${osName}", "Test Report")
                             GithubNotificator.updateStatus("Test", "${asicName}-${osName}-${test}", "success", options, NotificationConfiguration.TEST_PASSED, "${BUILD_URL}/${test.replace("_", "_5f")}_20report")
