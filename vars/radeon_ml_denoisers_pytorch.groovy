ac450a6efd5ddbd277cd6dfe1c07c58847a75a47
jakubR1 <jakub.rybski@dxc.com>
2022-08-08 11:51:59 +0200

    fix  bug

--- vars/radeon_ml_denoisers_pytorch.groovy
+++ vars/radeon_ml_denoisers_pytorch.groovy
@@ -41,7 +41,7 @@ def executeTestCommand(String osName, String asicName, Map options)
                         println "[INFO] Current test: ${test}.py"
 
                         sh  """
-                                expect sh/start_test.exp ${test}" >> ../${STAGE_NAME}_${test}.log 2>&1
+                                expect sh/start_test.exp ${test} >> ../${STAGE_NAME}_${test}.log 2>&1
                             """
                             utils.publishReport(this, BUILD_URL, "tested", "tested_${test}.html", "${test} report ${osName}", "Test Report")
                             GithubNotificator.updateStatus("Test", "${asicName}-${osName}-${test}", "success", options, NotificationConfiguration.TEST_PASSED, "${BUILD_URL}/${test.replace("_", "_5f")}_20report")
